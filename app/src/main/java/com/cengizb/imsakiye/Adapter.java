d669d036e046e1901f5a2f5a2b22fc03bf252ba7
cngzb <cngzbayrak@gmail.com>
2020-04-24 20:48:06 +0300

    migrated to androidx

--- app/src/main/java/com/cengizb/imsakiye/Adapter.java
+++ app/src/main/java/com/cengizb/imsakiye/Adapter.java
@@ -2,13 +2,12 @@ package com.cengizb.imsakiye;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
